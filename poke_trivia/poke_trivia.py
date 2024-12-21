4fd3bfa33a8aee30a61e8d0d18515cad97d892d7
brunodantas <bruno.welldm@gmail.com>
2024-12-21 10:27:26 -0300

    cache content

--- poke_trivia/poke_trivia.py
+++ poke_trivia/poke_trivia.py
@@ -1,4 +1,5 @@
 import datetime
+from functools import lru_cache
 import re
 from itertools import chain
 
@@ -9,6 +10,49 @@ from bs4 import BeautifulSoup
 from rxconfig import config
 
 
+def get_pokemon_url(date):
+    response = requests.get(
+        "https://bulbapedia.bulbagarden.net/wiki/"
+        "List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number"
+    )
+    soup = BeautifulSoup(response.text, "html.parser")
+    table_contents = (
+        c or ""
+        for c in chain.from_iterable(table.contents for table in soup.find_all("table"))
+        if not c.string
+    )
+    urls = list(
+        set(
+            re.findall(
+                "(/wiki/.*_\(Pok%C3%A9mon\))",
+                " ".join(str(c) for c in table_contents),
+            )
+        )
+    )
+    url = urls[hash(date) % len(urls)]
+    return url
+
+
+@lru_cache(1)
+def get_pokemon_by_date(date):
+    url = "https://bulbapedia.bulbagarden.net" + get_pokemon_url((date))
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    image_tag = soup.find_all("img")[2]
+    image = image_tag["src"]
+    title = " ".join(str(soup.find("h1").string).split()[:-1])
+    tag = soup.find("h2", string="Trivia")
+    content = []
+    if tag:
+        for c in tag.find_next_siblings():
+            if c and c.name == "h2":
+                break
+            content.append(str(c))
+    content = "".join(content)
+    content = content.replace("/wiki/", "https://bulbapedia.bulbagarden.net/wiki/")
+    return title, image, content
+
+
 class State(rx.State):
     """The app state."""
 
@@ -16,49 +60,9 @@ class State(rx.State):
     image = ""
     content = ""
 
-    def get_pokemon_url(self):
-        response = requests.get(
-            "https://bulbapedia.bulbagarden.net/wiki/"
-            "List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number"
-        )
-        soup = BeautifulSoup(response.text, "html.parser")
-        table_contents = (
-            c or ""
-            for c in chain.from_iterable(
-                table.contents for table in soup.find_all("table")
-            )
-            if not c.string
-        )
-        urls = list(
-            set(
-                re.findall(
-                    "(/wiki/.*_\(Pok%C3%A9mon\))",
-                    " ".join(str(c) for c in table_contents),
-                )
-            )
-        )
-        today = datetime.datetime.today().date()
-        url = urls[hash(today) % len(urls)]
-        return url
-
     def get_pokemon_data(self):
-        url = "https://bulbapedia.bulbagarden.net" + self.get_pokemon_url()
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        image_tag = soup.find_all("img")[2]
-        self.image = image_tag["src"]
-        self.title = " ".join(str(soup.find("h1").string).split()[:-1])
-        tag = soup.find("h2", string="Trivia")
-        content = []
-        if tag:
-            for c in tag.find_next_siblings():
-                if c and c.name == "h2":
-                    break
-                content.append(str(c))
-        self.content = "".join(content)
-        self.content = self.content.replace(
-            "/wiki/", "https://bulbapedia.bulbagarden.net/wiki/"
-        )
+        today = datetime.datetime.today().date()
+        self.title, self.image, self.content = get_pokemon_by_date(today)
 
 
 @rx.page(on_load=State.get_pokemon_data)
